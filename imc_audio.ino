49f9a7ed7d67762a6aab08b5e6e5a88bc3cc0572
Ping123 <patzschke@the-case.org>
2023-11-30 14:27:00 +0100

    Update imc_audio.ino

--- imc_audio.ino
+++ imc_audio.ino
@@ -6,17 +6,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <Wire.h>                 // Communication between Arduinos
+
 // define Pins & max. LED's --------------------------------------------------------------------------------------------
-#define SPEAKER        46         // Speaker Pin
-#define SDPIN          53         // SD-Card-Slot Pin
+#define SPEAKER           46         // Speaker Pin
+#define SDPIN             53         // SD-Card-Slot Pin
+volatile bool data_flag   = false;
+
 
 // initial Setup -------------------------------------------------------------------------------------------------------
-
 TMRpcm audio;
 File root;
 
 void setup() {
   
+  Wire.begin(8);                  // join i2c bus with address #8
+  Wire.onReceive(receiveEvent);   // register event
+
   Serial.begin(9600);
   Serial.print("Initializing SD card...");
 
@@ -29,15 +35,46 @@ void setup() {
   audio.speakerPin = SPEAKER;
   audio.setVolume(3);
   audio.quality(0);
-
-  delay(1000);
   Serial.println("init done");
 }
 
+// function that executes whenever data is received from master --------------------------------------------------------
+void receiveEvent(int bytes) {
+  data_flag = true;
+}
+
+/*
+void receiveEvent(int howMany) {
+  while(1 < Wire.available()) {
+    char c = Wire.read();         // receive byte as a character
+    Serial.print(c);              // print the character
+  }
+  int x = Wire.read();            // receive byte as an integer
+  Serial.println(x);              // print the integer
+}
+*/
+
 // MAIN LOOP FUNCTION --------------------------------------------------------------------------------------------------
 void loop() {
+
+  if (data_flag == true) {
+    int go = Wire.read();
+    Serial.println(go);
+    
+    if (go == 1) {
+        audio.play("imc_2.wav");
+        data_flag = false;
+      } else {
+        data_flag = false;
+    }
+  }
+
+  delay(500);
+
+  /*
   Serial.println("loop start");
   audio.play("imc_2.wav");
   delay(119000);
   Serial.println("loop end");
+  */
 }
